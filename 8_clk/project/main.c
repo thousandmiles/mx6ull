0b4171f8a236888471714f99ac8b5258a186e750
thousandmiles <longdaluoshen@gmail.com>
2023-10-09 23:38:01 +0800

    new main function

--- 8_clk/project/main.c
+++ 8_clk/project/main.c
@@ -1,95 +1,49 @@
+#include "bsp_clk.h"
+#include "bsp_delay.h"
+#include "bsp_led.h"
+#include "bsp_beep.h"
+#include "bsp_key.h"
+
 /*
-author: forrest long and openedv
-
-project: led lighting
-
-version: 1.0
-
-description: c file for led lighting using struct official headers
-
-arm: I.MX6ULL
-*/
-
-#include "fsl_iomuxc.h"
-#include "fsl_common.h"
-
-
-void clk_enable(void)
-{
-	CCM->CCGR0 = 0XFFFFFFFF;
-	CCM->CCGR1 = 0XFFFFFFFF;
-
-	CCM->CCGR2 = 0XFFFFFFFF;
-	CCM->CCGR3 = 0XFFFFFFFF;
-	CCM->CCGR4 = 0XFFFFFFFF;
-	CCM->CCGR5 = 0XFFFFFFFF;
-	CCM->CCGR6 = 0XFFFFFFFF;
-}
-
-void led_init(void)
-{
-	// IOMUX_SW_MUX->GPIO1_IO03 = 0X5;	
-
-	IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03, 0);
-
-    /*
-    Configure the GPIO1_IO03: IOMUXC_SW_PAD_CTL_PAD_GPIO1_IO03
-    --bit [16]:0            HYS: disabled
-    --bit [15:14]:00        PUS: pull down enable
-    --bit [13]:0            PUE: keeper status
-    --bit [12]:1            PKE: pull/keeper enable
-    --bit [11]:0            ODE: open circuit disabled
-    --bit [7:6]:10          SPEED: moderate
-    --bit [5:3]:110         DSE: R0/R6
-    --bit [0]:0             SRE: low rate
-
-    bit[16:0]: 0    0   0   0   1   0   0   0   0   1   0   1   1   0   0   0   0
-    bit        16   15  14  13  12  11  10  9   8   7   6   5   4   3   2   1   0
-               0    -------1------  ------0------   ------B------   ------0------
-    --> 10B0
-    */
-    IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03, 0X10B0);
-
-	GPIO1->GDIR = 0X0000008;
-
-	GPIO1->DR &= ~(1 << 3);
-}
-
-void led_on(void)
-{
-	GPIO1->DR &= ~(1<<3); 
-}
-
-void led_off(void)
-{
-	GPIO1->DR |= (1<<3); 
-}
-
-void delay_short(volatile unsigned int n)
-{
-	while(n--){}
-}
-
-void delay(volatile unsigned int n)
-{
-	while(n--)
-	{
-		delay_short(0x7ff);
-	}
-}
-
+ * @description	: main函数
+ * @param 		: 无
+ * @return 		: 无
+ */
 int main(void)
 {
-	clk_enable();	
-	led_init();		
+	int i = 0;
+	int keyvalue = 0;
+	unsigned char led_state = OFF;
+	unsigned char beep_state = OFF;
 
-	while(1)	
+	imx6u_clkinit();	/* 初始化系统时钟 			*/
+	clk_enable();		/* 使能所有的时钟 			*/
+	led_init();			/* 初始化led 			*/
+	beep_init();		/* 初始化beep	 		*/
+	key_init();			/* 初始化key 			*/
+
+	while(1)			
 	{	
-		led_off();	
-		delay(100);		
+		keyvalue = key_get_value();
+		if(keyvalue)
+		{
+			switch ((keyvalue))
+			{
+				case KEY0_VALUE:
+					beep_state = !beep_state;
+					beep_switch(beep_state);
+					break;
+			}
+		}
+		i++;
+		if(i==50)
+		{
+			i = 0;
+			led_state = !led_state;
+			led_switch(LED0, led_state);
+		}
 
-		led_on();		
-		delay(1000);		
+		delayer(10);
 	}
 
 	return 0;
