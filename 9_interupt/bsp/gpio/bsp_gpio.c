94b9b16bd2a2bc96e4795fdd741b678046bdb116
thousandmiles <longdaluoshen@gmail.com>
2023-10-09 23:38:55 +0800

    enable GPIO18 interrupt

--- 9_interupt/bsp/gpio/bsp_gpio.c
+++ 9_interupt/bsp/gpio/bsp_gpio.c
@@ -12,6 +12,8 @@ gpio_pinwrite()     - Write value to GPIO
 
 void gpio_init(GPIO_Type *base, int pin, gpio_pin_config_t* config)
 {
+    gpio_disableint(base, pin);                         // disable interrupt
+
     if (config->direction == kGPIO_DigitalInput)        // digital input
     {
         base->GDIR &= ~(1<<pin);
@@ -21,6 +23,8 @@ void gpio_init(GPIO_Type *base, int pin, gpio_pin_config_t* config)
         base->GDIR |= (1<<pin);
         gpio_pinwrite(base, pin, config->outputLogic);  // default output
     }
+
+    gpio_intconfig(base, pin, config->interruptMode);
 }
 
 // read DR register value
@@ -39,4 +43,69 @@ void gpio_pinwrite(GPIO_Type *base, int pin, int value)
     {
         base->DR |= (1<<pin);   // output 1
     }
+}
+
+void gpio_intconfig(GPIO_Type* base, uint16_t pin, gpio_interupt_mode_t pinInterruptMode)
+{
+    volatile uint32_t* icr;
+    uint32_t icrShift;
+
+    icrShift = pin;
+
+    base->EDGE_SEL &= ~(1<<pin);
+
+    if (pin < 16)   // ICR1 register
+    {
+        icr = &(base->ICR1);
+    }
+    else
+    {
+        icr = &(base->ICR2);
+        icrShift -= 16;
+    }
+
+    switch (pinInterruptMode)
+    {
+        case(kGPIO_IntLowLevel):
+        {
+            *icr &= ~(3<<(2*icrShift));
+            break;
+        }
+        case(kGPIO_IntHighLevel):
+        {
+            *icr = (*icr & (~(3<<(2*icrShift)))) | (1<<(2*icrShift));
+            break;
+        }
+        case(kGPIO_IntRisingEdge):
+        {
+            *icr = (*icr & (~(3<<(2*icrShift)))) | (2<<(2*icrShift));
+            break;
+        }
+        case(kGPIO_IntFallingEdge):
+        {
+            *icr |= (3<<(2*icrShift));
+            break;
+        }
+        case(kGPIO_IntRisingOrFallingEdge):
+        {
+            base->EDGE_SEL |= (1<<pin);
+        }
+        default:
+            break;
+    }
+}
+
+void gpio_enableint(GPIO_Type* base, uint16_t pin)
+{
+    base->IMR |= (1<<pin);
+}
+
+void gpio_disableint(GPIO_Type* base, uint16_t pin)
+{
+    base->IMR &= ~(1<<pin);
+}
+
+void gpio_clearintflags(GPIO_Type* base, uint16_t pin)
+{
+    base->ISR |= (1<<pin);
 }
--- 9_interupt/bsp/gpio/bsp_gpio.h
+++ 9_interupt/bsp/gpio/bsp_gpio.h
@@ -10,14 +10,37 @@ typedef enum _gpio_pin_direction
 }gpio_pin_direction_t;
 
 
+/* interupt enum */
+typedef enum _gpio_interupt_mode
+{
+    kGPIO_NoIntmode = 0U,       /* no interupt */
+    kGPIO_IntLowLevel = 1U, 
+    kGPIO_IntHighLevel = 2U,
+    kGPIO_IntRisingEdge = 3U,
+    kGPIO_IntFallingEdge = 4U,
+    kGPIO_IntRisingOrFallingEdge = 5U,
+}gpio_interupt_mode_t;
+
+
 typedef struct _gpio_pin_config
 {
     gpio_pin_direction_t direction;
     uint8_t outputLogic;
+    gpio_interupt_mode_t interruptMode;
 }gpio_pin_config_t;
 
 void gpio_init(GPIO_Type* base, int pin, gpio_pin_config_t* config);
+
 int gpio_pinread(GPIO_Type* base, int pin);
+
 void gpio_pinwrite(GPIO_Type* base, int pin, int value);
+
+void gpio_intconfig(GPIO_Type* base, uint16_t pin, gpio_interupt_mode_t pinInterruptMode);
+
+void gpio_enableint(GPIO_Type* base, uint16_t pin);
+
+void gpio_disableint(GPIO_Type* base, uint16_t pin);
+
+void gpio_clearintflags(GPIO_Type* base, uint16_t pin);
 
 #endif // !
