3bbf31fa494739012d8b592763f6263e9efb9b8a
thousandmiles <longdaluoshen@gmail.com>
2023-10-18 22:09:29 +0800

    high precise delayer

--- 12_highpreci_delay/bsp/delay/bsp_delay.h
+++ 12_highpreci_delay/bsp/delay/bsp_delay.h
@@ -5,4 +5,12 @@
 
 void delayer(volatile unsigned int n);
 
+void delay_init(void);
+
+void delay_us(uint32_t usdelay);
+
+void delay_ms(uint32_t msdelay);
+
+#define MAX_CNT 0XFFFFFFFF
+
 #endif // !__BSP_DELAY_H
