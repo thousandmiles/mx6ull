9d00ab02e362965196e8333cb19c072d0a3eef37
thousandmiles <longdaluoshen@gmail.com>
2023-11-03 23:23:57 +0800

    use new api

--- 14_lcd/project/main.c
+++ 14_lcd/project/main.c
@@ -4,11 +4,14 @@
 #include "bsp_beep.h"
 #include "bsp_key.h"
 #include "bsp_int.h"
-#include "bsp_exit.h"
-#include "bsp_epittimer.h"
-#include "bsp_keyfilter.h"
 #include "bsp_uart.h"
+#include "bsp_lcd.h"
+#include "bsp_lcdapi.h"
 #include "/home/long/src/mx6ull/13_uart/stdio/include/stdio.h"
+
+
+unsigned int BACKCOLOR[6] = {LCD_BLUE, LCD_GREEN, LCD_RED, LCD_ORANGE, LCD_BLACK, LCD_WHITE};
+
 
 /*
  * @description	: main函数
@@ -18,52 +21,46 @@
 int main(void)
 {
 
-	uint8_t state = OFF;
+	unsigned char index = 0;
+	unsigned char state = OFF;
 
 	int_init(); 					/* initialize system interrupt		*/
 	imx6u_clkinit();				/* initialize system clock 			*/
-	clk_enable();					/* enable all clocks 				*/
-	led_init();						/* led 								*/
-	beep_init();					/* beep	 							*/
-	//key_init();						/* key 								*/
-	//epit1_init(0, 66000000*2);		/* clock interrupt					*/
-	//filterkey_init();				/* key with timer anti-shake*/
-	uart_init();                    /* uart */
+    delay_init();
+	clk_enable();
+	led_init();
+	beep_init();
+	uart_init();
+	lcd_init();
 
-	// (1) single char input / output
-	/*
-	uint8_t a = 0;
+	tftlcd_dev.forecolor = LCD_RED;
 
-	while(1)			
-	{	
-		put_a_str(" Please input a char: ");
-		a = get_a_char();
-		put_a_char(a);
-		put_a_str("\r\n");
+	lcd_show_string(10, 10, 400, 32, 32, (char*)"ALPHA-IMX6UL ELCD TEST");
 
-		put_a_str("You have input the: ");
-		put_a_char(a);
-		put_a_str("\r\n\r\n");
+	lcd_draw_rectangle(10, 52, 1014, 290);
 
-		state =!state;
-		led_switch(LED0, state);
-	}
-	*/
+	lcd_drawline(10, 52, 1014, 290);
 
-	// (2) printf / scanf
-	int a = 0;
-	int b = 0;
-	while (1)
+	lcd_drawline(10, 290, 1014, 52);
+
+	lcd_draw_Circle(512, 171, 119);
+
+	while(1)
 	{
-		printf("input two int numbers, using space separated: ");
-		scanf("%d %d", &a, &b);
+		index++;
+		if(index == 6) index = 0;
 
-		printf("\r\n %d + %d = %d \r\n", a, b, a + b);
+		lcd_fill(0, 300, 1023, 599, BACKCOLOR[index]);
 
-		state =!state;
+		lcd_show_string(800, 10, 240, 32, 32, (char*)"INDEX = ");
+
+		lcd_shownum(896, 10, index, 2, 32);
+
+		state = !state;
 		led_switch(LED0, state);
-
+		delay_ms(1000);
 	}
+	
 
 	return 0;
 }
