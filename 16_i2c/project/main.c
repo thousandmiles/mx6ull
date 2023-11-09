4a6edbe758698b429ce5527909ee3e15eb968a3f
thousandmiles <longdaluoshen@gmail.com>
2023-11-10 00:17:19 +0800

    main function

--- 16_i2c/project/main.c
+++ 16_i2c/project/main.c
@@ -8,6 +8,7 @@
 #include "bsp_lcd.h"
 #include "bsp_lcdapi.h"
 #include "bsp_rtc.h"
+#include "bsp_ap3216c.h"
 #include "/home/long/src/mx6ull/13_uart/stdio/include/stdio.h"
 
 
@@ -22,11 +23,8 @@ unsigned int BACKCOLOR[6] = {LCD_BLUE, LCD_GREEN, LCD_RED, LCD_ORANGE, LCD_BLACK
 int main(void)
 {
 	
-	unsigned char key = 0;
-	int t = 0;
-	int i = 3;
-	char buf[160];
-	struct rtc_datetime rtcdate;
+	unsigned short ir, als, ps;
+
 	unsigned char state = OFF;
 
 	// initialze
@@ -39,81 +37,55 @@ int main(void)
 	beep_init();
 	uart_init();
 	lcd_init();
-	rtc_init();
 
 	tftlcd_dev.forecolor = LCD_RED;
 
-	lcd_show_string(50, 350, 400, 24, 24, (char*)"ALPHA-IMX6UL RTC TEST");
+	lcd_show_string(30, 50, 200, 16, 16, (char*)"ALPHA-IMX6UL IIC TEST");
 
-	tftlcd_dev.forecolor = LCD_BLUE;
+	lcd_show_string(30, 70, 200, 16, 16, (char*)"AP3216C TEST");
 
-	lcd_draw_rectangle(10, 52, 1014, 290);
+	lcd_show_string(30, 90, 200, 16, 16, (char*)"ATOM@ALIENTEK");
 
-	lcd_drawline(10, 52, 1014, 290);
+	lcd_show_string(30, 110, 200, 16, 16, (char*)"2023/11/09");
 
-	lcd_drawline(10, 290, 1014, 52);
 
-	lcd_draw_Circle(512, 171, 119);
-
-	memset(buf, 0, sizeof(buf));
-
-	while(1)
+	while (ap3216c_init())
 	{
-		if (t == 100)
-		{
-			t = 0;
-			//printf("will be running %d s......\r", i);
-			lcd_fill(50, 370, 400, 400, tftlcd_dev.backcolor);
-			sprintf(buf, "will be running %ds......", i);
-			lcd_show_string(50, 370, 300, 24, 24, buf);
-			i--;
-			if(i < 0) break;
-		}
+		lcd_show_string(30, 130, 200, 16, 16, (char*)"AP3216C Check Failed!");
 
-		key = key_get_value();
+		delay_ms(500);
 
-		if (key == KEY0_VALUE)
-		{
-			rtcdate.year = 2023;
-			rtcdate.month = 11;
-			rtcdate.day = 5;
-			rtcdate.hour = 15;
-			rtcdate.minute = 50;
-			rtcdate.second = 0;
+		lcd_show_string(30, 130, 200, 16, 16, (char*)"Please Check         ");
 
-			rtc_setdatetime(&rtcdate);
-			lcd_fill(50, 370, 400, 400, tftlcd_dev.backcolor);
-			lcd_show_string(50, 370, 300, 24, 24, (char*)"\r\nRTC Init finished!\r\n");
-		}
-
-		delay_ms(10);
-		t++;
+		delay_ms(500);
 	}
 
-	tftlcd_dev.forecolor = LCD_RED;
-	lcd_fill(50, 370, 400, 400, tftlcd_dev.backcolor);
+	lcd_show_string(30, 130, 200, 16, 16, (char*)"AP3216C Ready!");
+
+	lcd_show_string(30, 160, 200, 16, 16, (char*)"IR: ");
+
+	lcd_show_string(30, 180, 200, 16, 16, (char*)"PS: ");
+
+	lcd_show_string(30, 200, 200, 16, 16, (char*)"ALS: ");
 	
-	lcd_show_string(50, 370, 300, 24, 24, (char*)"Current Time:");
 	tftlcd_dev.forecolor = LCD_BLUE;
 
 	while (1)
 	{
-		rtc_getdatetime(&rtcdate);
-		sprintf(buf, "%d/ %d/ %d %d: %d: %d", 	rtcdate.year,
-												rtcdate.month,
-												rtcdate.day,
-												rtcdate.hour,
-												rtcdate.minute,
-												rtcdate.second);
-		lcd_fill(50, 400, 400, 400, tftlcd_dev.backcolor);
-		lcd_show_string(50, 400, 250, 24, 24, buf);
+		ap3216c_readdata(&ir, &ps, &als);
+
+		lcd_shownum(30 + 32, 160, ir, 5, 16);
+
+		lcd_shownum(30 + 32, 180, ps, 5, 16);
+
+		lcd_shownum(30 + 32, 200, als, 5, 16);
+		
+		delay_ms(120);
 
 		state = !state;
+
 		led_switch(LED0, state);
-		delay_ms(1000);
 	}
-
-
 
 	return 0;
 }
