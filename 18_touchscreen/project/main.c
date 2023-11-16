d864a03554a0b8b4ee524f6e7d925af19251eacf
thousandmiles <longdaluoshen@gmail.com>
2023-11-16 23:48:56 +0800

    display touch point

--- 18_touchscreen/project/main.c
+++ 18_touchscreen/project/main.c
@@ -8,70 +8,9 @@
 #include "bsp_lcd.h"
 #include "bsp_lcdapi.h"
 #include "bsp_rtc.h"
-#include "bsp_ap3216c.h"
-#include "bsp_icm20608.h"
-#include "bsp_spi.h"
+#include "bsp_ft5xx6.h"
+#include "bsp_gt9147.h"
 #include "/home/long/src/mx6ull/13_uart/stdio/include/stdio.h"
- 
-
-unsigned int BACKCOLOR[6] = {LCD_BLUE, LCD_GREEN, LCD_RED, LCD_ORANGE, LCD_BLACK, LCD_WHITE};
-
-
-void integer_display(unsigned short x, unsigned short y, unsigned char size, signed int num)
-{
-	char buf[200];
-
-	lcd_fill(x, y, x + 50, y + size, tftlcd_dev.backcolor);
-
-	memset(buf, 0, sizeof(buf));
-
-	if (num < 0)
-	{
-		sprintf(buf, "-%d", -num);
-	}
-	else
-	{
-		sprintf(buf, "%d", num);
-	}
-
-	lcd_show_string(x, y, 50, size, size, buf);
-}
-
-
-void decimals_display(unsigned short x, unsigned short y, unsigned char size, signed int num)
-{
-	signed int integ;
-
-	signed int fract;
-
-	signed int uncomptemp = num;
-
-	char buf[200];
-
-	if (num < 0)
-	{
-		uncomptemp = -uncomptemp;
-	}
-
-	integ = uncomptemp /100;
-
-	fract = uncomptemp % 100;
-
-	memset(buf, 0, sizeof(buf));
-
-	if (num < 0)
-	{
-		sprintf(buf, "-%d.%d", integ, fract);
-	}
-	else
-	{
-		sprintf(buf, "%d.%d", integ, fract);
-	}
-
-	lcd_fill(x, y, x + 60, y + size, tftlcd_dev.backcolor);
-
-	lcd_show_string(x, y, 60, size, size, buf);
-}
 
 
 void imx6ul_hardfpu_enable(void)
@@ -102,6 +41,7 @@ void imx6ul_hardfpu_enable(void)
 int main(void)
 {
 
+	unsigned char i = 0;
 	unsigned char state = OFF;
 
 	// initialze
@@ -114,107 +54,113 @@ int main(void)
 	beep_init();
 	uart_init();
 	lcd_init();
+	gt9147_init();
+
+	if (gt_init_fail == 1)
+	{
+		ft5426_init();
+	}
 
 	tftlcd_dev.forecolor = LCD_RED;
 
-	lcd_show_string(50, 10, 400, 24, 24, (char*)"ALPHA-IMX6UL SPI TEST");
+	lcd_show_string(50, 10, 400, 24, 24, (char*)"ALPHA-IMX6UL TOUCH SCREEN TEST");
 
-	lcd_show_string(50, 40, 200, 16, 16, (char*)"ICM20608 TEST");
+	lcd_show_string(50, 40, 200, 16, 16, (char*)"TOUCH SCREEN TEST");
 
 	lcd_show_string(50, 60, 200, 16, 16, (char*)"ATOM@ALIENTEK");
 
-	lcd_show_string(50, 80, 200, 16, 16, (char*)"2023/11/13");
+	lcd_show_string(50, 80, 200, 16, 16, (char*)"2023/11/16");
 
 
-	while (icm20608_init())
-	{
-		lcd_show_string(50, 100, 200, 16, 16, (char*)"ICM20608 Check Failed!");
+	lcd_show_string(50, 130, 400, 16, 16, (char*)"TP Num: ");
 
-		delay_ms(500);
+	lcd_show_string(50, 150, 200, 16, 16, (char*)"Point0 X: ");
 
-		lcd_show_string(50, 100, 200, 16, 16, (char*)"Please Check         ");
+	lcd_show_string(50, 170, 200, 16, 16, (char*)"Point0 Y: ");
 
-		delay_ms(500);
-	}
+	lcd_show_string(50, 190, 200, 16, 16, (char*)"Point1 X: ");
 
+	lcd_show_string(50, 210, 200, 16, 16, (char*)"Point1 Y: ");
 
-	lcd_show_string(50, 100, 200, 16, 16, (char*)"ICM20608 Ready!");
-	
+	lcd_show_string(50, 230, 200, 16, 16, (char*)"Point2 X: ");
 
+	lcd_show_string(50, 250, 200, 16, 16, (char*)"Point2 Y: ");
 
-	lcd_show_string(50, 130, 200, 16, 16, (char*)"accel x: ");
+	lcd_show_string(50, 270, 200, 16, 16, (char*)"Point3 X: ");
 
-	lcd_show_string(50, 150, 200, 16, 16, (char*)"accel y: ");
+	lcd_show_string(50, 290, 200, 16, 16, (char*)"Point3 Y: ");
 
-	lcd_show_string(50, 170, 200, 16, 16, (char*)"accel z: ");
+	lcd_show_string(50, 310, 200, 16, 16, (char*)"Point4 X: ");
 
-	lcd_show_string(50, 190, 200, 16, 16, (char*)"gyro  x: ");
-
-	lcd_show_string(50, 210, 200, 16, 16, (char*)"gyro  x: ");
-
-	lcd_show_string(50, 230, 200, 16, 16, (char*)"gyro  z: ");
-
-	lcd_show_string(50, 250, 200, 16, 16, (char*)"temp   : ");
-
-
-
-	lcd_show_string(50 + 181, 130, 200, 16, 16, (char*)"g");
-
-	lcd_show_string(50 + 181, 150, 200, 16, 16, (char*)"g");
-
-	lcd_show_string(50 + 181, 170, 200, 16, 16, (char*)"g");
-
-	lcd_show_string(50 + 181, 190, 200, 16, 16, (char*)"o/s");
-
-	lcd_show_string(50 + 181, 210, 200, 16, 16, (char*)"o/s");
-
-	lcd_show_string(50 + 181, 230, 200, 16, 16, (char*)"o/s");
-
-	lcd_show_string(50 + 181, 250, 200, 16, 16, (char*)"C");
-
+	lcd_show_string(50, 330, 200, 16, 16, (char*)"Point4 Y: ");
 
 	tftlcd_dev.forecolor = LCD_BLUE;
 
 	while(1)
 	{
 
-		icm20608_getdata();
+		if (gt_init_fail == 1)
+		{
+			lcd_shownum(50 + 72, 130, ft5426_dev.point_num, 1, 16);
 
-		integer_display(50 + 70, 130, 16, icm20608_dev.accel_x_adc);
+			lcd_shownum(50 + 72, 150, ft5426_dev.x[0], 5, 16);
 
-		integer_display(50 + 70, 150, 16, icm20608_dev.accel_y_adc);
+			lcd_shownum(50 + 72, 170, ft5426_dev.y[0], 5, 16);
 
-		integer_display(50 + 70, 170, 16, icm20608_dev.accel_z_adc);
+			lcd_shownum(50 + 72, 190, ft5426_dev.x[1], 5, 16);
 
-		integer_display(50 + 70, 190, 16, icm20608_dev.gyro_x_adc);
+			lcd_shownum(50 + 72, 210, ft5426_dev.y[1], 5, 16);
 
-		integer_display(50 + 70, 210, 16, icm20608_dev.gyro_y_adc);
+			lcd_shownum(50 + 72, 230, ft5426_dev.x[2], 5, 16);
 
-		integer_display(50 + 70, 230, 16, icm20608_dev.gyro_z_adc);
+			lcd_shownum(50 + 72, 250, ft5426_dev.y[2], 5, 16);
 
-		integer_display(50 + 70, 250, 16, icm20608_dev.temp_adc);
+			lcd_shownum(50 + 72, 270, ft5426_dev.x[3], 5, 16);
+
+			lcd_shownum(50 + 72, 290, ft5426_dev.y[3], 5, 16);
+
+			lcd_shownum(50 + 72, 310, ft5426_dev.x[4], 5, 16);
+
+			lcd_shownum(50 + 72, 330, ft5426_dev.y[4], 5, 16);
+		}
+		else
+		{
+			lcd_shownum(50 + 72, 130, gt9147_dev.point_num, 1, 16);
+
+			lcd_shownum(50 + 72, 150, gt9147_dev.x[0], 5, 16);
+
+			lcd_shownum(50 + 72, 170, gt9147_dev.y[0], 5, 16);
+
+			lcd_shownum(50 + 72, 190, gt9147_dev.x[1], 5, 16);
+
+			lcd_shownum(50 + 72, 210, gt9147_dev.y[1], 5, 16);
+
+			lcd_shownum(50 + 72, 230, gt9147_dev.x[2], 5, 16);
+
+			lcd_shownum(50 + 72, 250, gt9147_dev.y[2], 5, 16);
+
+			lcd_shownum(50 + 72, 270, gt9147_dev.x[3], 5, 16);
+
+			lcd_shownum(50 + 72, 290, gt9147_dev.y[3], 5, 16);
+
+			lcd_shownum(50 + 72, 310, gt9147_dev.x[4], 5, 16);
+
+			lcd_shownum(50 + 72, 330, gt9147_dev.y[4], 5, 16);
+		}
 
 
+		delay_ms(10);
 
-		integer_display(50 + 70 + 50, 130, 16, icm20608_dev.accel_x_act);
+		++i;
 
-		integer_display(50 + 70 + 50, 150, 16, icm20608_dev.accel_y_act);
+		if (i == 50)
+		{
+			i = 0;
 
-		integer_display(50 + 70 + 50, 170, 16, icm20608_dev.accel_z_act);
+			state = !state;
 
-		integer_display(50 + 70 + 50, 190, 16, icm20608_dev.gyro_x_act);
-
-		integer_display(50 + 70 + 50, 210, 16, icm20608_dev.gyro_y_act);
-
-		integer_display(50 + 70 + 50, 230, 16, icm20608_dev.gyro_z_act);
-
-		integer_display(50 + 70 + 50, 250, 16, icm20608_dev.temp_act);
-
-
-
-		delay_ms(120);
-		state = !state;
-		led_switch(LED0, state);
+			led_switch(LED0, state);
+		}
 	
 	}
 
